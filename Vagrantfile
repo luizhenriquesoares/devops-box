0939a1f2fade137c371914b66801e174e234bfeb
luiz henrique soares <luizhenriquesoares91@gmail.com>
2018-11-22 17:10:40 -0300

    install kubernetes

--- Vagrantfile
+++ Vagrantfile
@@ -1,9 +1,9 @@
 Vagrant.configure(2) do |config|
 	config.vm.define "devops-box" do |devbox|
-		devbox.vm.box = "ubuntu/xenial64"
-    		#devbox.vm.network "private_network", ip: "192.168.199.9"
-    		#devbox.vm.hostname = "devops-box"
-      		devbox.vm.provision "shell", path: "scripts/install.sh"
+		devbox.vm.box = "envimation/ubuntu-xenial-docker"
+    		#devbox.vm.network "public_network", ip: "192.168.0.4", bridge: "br-10611e8c8f5f"
+    		devbox.vm.hostname = "devops-box"
+      	devbox.vm.provision "shell", path: "scripts/install.sh"
     		devbox.vm.provider "virtualbox" do |v|
     		  v.memory = 4096
     		  v.cpus = 2
--- scripts/install.sh
+++ scripts/install.sh
@@ -43,6 +43,13 @@ T_RETVAL=${PIPESTATUS[0]}
 && unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
 && rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 
+## Instal Kubernetes
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
 ## Instal Kops
 curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64 \
 & chmod +x kops-linux-amd64 \
